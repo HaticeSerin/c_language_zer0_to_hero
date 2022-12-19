eb0a42e681ca5e2f5adae7a58ad3fb0aec522572
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-19 21:54:58 +0300

    Hatice Serin
    @themanoftalent

--- typedef.c
+++ typedef.c
@@ -1,19 +1,15 @@
 
 // typedef with array
+#include"stdio.h"
 
-#include <stdio.h>
-
-typedef int Arr[5];
-
-
-int main()
-{
-Arr temp = {10, 20, 30, 40,50};
-printf("typedef using an array\n");
-	
-for (int i = 0; i < 5; i++)
-{
-	printf("%d ", temp[i]);
-}
-return 0;
+typedef int Arr[3];
+int main(){
+	int i;
+	Arr temp={27,30,33};
+	printf("typedef using an array\n");
+	for(i=0;i<3;i++){
+		printf("%d ",temp[i]);
+	   		
+	}
+	return 0;
 }

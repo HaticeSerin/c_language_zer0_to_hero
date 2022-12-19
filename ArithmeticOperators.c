90efa07f66eb4367b5a40a594c4dd7987e5d9621
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-19 21:05:03 +0300

    Hatice Serin
    @themanoftalent

--- ArithmeticOperators.c
+++ ArithmeticOperators.c
@@ -5,19 +5,20 @@
 #include <stdio.h>
 int main()
 {
-    int a = 9, 
-        b = 4, 
-        c;
+    int a = 9; 
+    int b = 3; 
+    int c;
     
     c = a+b;
     printf("a+b = %d \n",c);
     c = a-b;
     printf("a-b = %d \n",c);
-    c = 
+    c =a*b;
     printf("a*b = %d \n",c);
     c = a/b;
     printf("a/b = %d \n",c);
-    c = 
+    c =b%a;
     printf("Remainder when a divided by b = %d \n",c);
     
     return 0;
+}

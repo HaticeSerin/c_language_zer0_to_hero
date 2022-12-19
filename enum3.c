15bda0883446069ef66c6e280e7f9d5b90b611f0
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-19 20:59:14 +0300

    Hatice Serin
    @themanoftalent

--- enum3.c
+++ enum3.c
@@ -1,14 +1,40 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include"stdio.h"
+#include"stdlib.h"
+enum months{January=1,February,March,April,May,June,July,August,September,October,November,December} i;
 
-enum month {
-    january = 1, february, march, april, may, june, july, august, september, october, november, december
-};
-
-int main() {
-    enum month ay;
-   for (ay = january; ay <= december; ay++) {
-       printf("%d \n", ay);
-    }
-    return 0;
+int main(){
+	int option;
+	printf("Enter a number (1-12):");
+	scanf("%d",&option);
+	i=option;
+	switch(i){
+		case 1:	printf("January");
+		break;
+		case 2:	printf("February");
+		break;
+		case 3: printf("March");
+		break;
+		case 4: printf("April");
+		break;
+		case 5: printf("May");
+		break;
+		case 6: printf("June");
+		break;
+		case 7: printf("July");
+		break;
+		case 8: printf("August");
+		break;
+		case 9: printf("September");
+		break;
+		case 10: printf("October");
+		break;
+		case 11: printf("November");
+		break;
+		case 12: printf("December");
+		break;
+		default:
+			printf("You have dialed wrong");
+			break;
+	}
+	return 0;
 }
